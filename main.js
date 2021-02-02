01854b99770bfe87248b9bd5f977cb58ef49f489
NellPoi <nellpoi@icloud.com>
2021-02-03 00:55:13 +0800

    修复：统一类名，新增接口
    启用了隐藏的调试接口，删除了稳定版本的调试信息，新增“onchange_Style_flexDirection”接口用于优化和同步option选项能够动态传递参数实现动态布局

--- main.js
+++ main.js
@@ -2,21 +2,33 @@ window.onload = function () {
     console.info("Loaded Script Successful")
 }
 
+function interface_OptionSync_flexFlow(v1,v2){
+    // let direction=document.getElementById("")
+    console.log(v1)
+}
+
 function onchange_Style_flexDirection() {
     let select = document.querySelector("#select_flex-direction")
     document.querySelector(".flex_table").style.flexDirection = select.value
-    console.info("flex-direction:", select.value)
+    // 镜像传递参数给对应接口
+    document.querySelector("#select_flex-flow_direction")[select.selectedIndex].selected = true
 }
 
-function onchange_Style_flexWarp() {
-    let select = document.querySelector("#select_flex-warp")
+function onchange_Style_flexWrap() {
+    let select = document.querySelector("#select_flex-wrap")
     document.querySelector(".flex_table").style.flexWrap = select.value;
-    console.info("flex-warp:", select.value)
+    document.querySelector("#select_flex-flow_wrap")[select.selectedIndex].selected = true;
+}
+
+function onchange_Style_flexFlow_Direction() {
+
+}
+
+function onchange_Style_flexFlow_Wrap() {
+
 }
 
 function test_Debug() {
-    console.debug(Date(), "| 您正在调用内建函数调试功能")
-    document.querySelector(".flex_table").style.flexWrap = "warp"
-    document.querySelector(".flex_table").style.flexWrap="wrap"
-    document.querySelector(".flex_table").setAttribute("flex-direction","column");
+    console.debug(Date(), "| 正在调用内建函数调试功能")
+
 }
