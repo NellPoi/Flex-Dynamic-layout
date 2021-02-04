eabdef4f9578dc6b27916c4ac8fdc442053cdc72
NellPoi <nellpoi@icloud.com>
2021-02-04 17:10:06 +0800

    新增：动态调整容器项目尺寸功能
    mainTitle类新增函数resetMainTitle()用于重置标题和重新给flex容器项目重新排序命名
    mainTitle类新增接口用于实时赋值新文本值
    新增两个控件用于控制flex容器每个项目的尺寸大小，以px为单位，1px*5倍率调整

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 window.onload = function () {
     console.info("Loaded Script Successful")
 }
-
+let table_Count=10;
+const title="Flex 布局应用"
 // function interface_OptionSync_flexFlow(v1, v2) {
 //     // let direction=document.getElementById("")
 //
@@ -21,35 +22,62 @@ function onchange_Style_flexWrap() {
 }
 
 function onchange_Style_flexFlow_Direction() {
-    let self=document.querySelector("#select_flex-flow_direction")
-    let partner=document.querySelector("#select_flex-flow_wrap")
-    let v1=self.value
-    let v2=partner.value
-    document.querySelector("#select_flex-direction").selectedIndex=self.selectedIndex
-    document.querySelector(".flex_table").style.flexFlow=v1+" "+v2
+    let self = document.querySelector("#select_flex-flow_direction")
+    let partner = document.querySelector("#select_flex-flow_wrap")
+    let v1 = self.value
+    let v2 = partner.value
+    document.querySelector("#select_flex-direction").selectedIndex = self.selectedIndex
+    document.querySelector(".flex_table").style.flexFlow = v1 + " " + v2
 }
 
 function onchange_Style_flexFlow_Wrap() {
-    let self=document.querySelector("#select_flex-flow_wrap")
-    let partner=document.querySelector("#select_flex-flow_direction")
-    let v1=self.value
-    let v2=partner.value
-    document.querySelector("#select_flex-wrap").selectedIndex=self.selectedIndex
-    document.querySelector(".flex_table").style.flexFlow=v1+" "+v2
+    let self = document.querySelector("#select_flex-flow_wrap")
+    let partner = document.querySelector("#select_flex-flow_direction")
+    let v1 = self.value
+    let v2 = partner.value
+    document.querySelector("#select_flex-wrap").selectedIndex = self.selectedIndex
+    document.querySelector(".flex_table").style.flexFlow = v1 + " " + v2
 }
-function oninput_Count(){
-
-    let value=document.querySelector("#range_Count").value
-
-    let element=document.createElement("div")
-    for(let i=1;i<=value;i++){
-        document.querySelector(".flex_table").appendChild(element)
-        element.innerText=i.toString()
+function oninput_Height(){
+    let value=document.querySelector("#range_Height").value
+    document.querySelector(".mainTitle").innerHTML=value+"%"
+    for (let i=1;i<=table_Count;i++) {
+        let obj = document.querySelector(".flex_table div:nth-child("+i+")")
+        obj.textContent=""
+        obj.style.height = (value*5) + "px"
+        obj.style.lineHeight=(value*5) + "px"
     }
 }
-// function onclick_RemoveAll(){
-//     let obj=document.querySelector(".flex_table")
-//     obj.parentNode.removeChild(obj)
-// }
+
+function oninput_Width(){
+    let value=document.querySelector("#range_Width").value
+    document.querySelector(".mainTitle").innerHTML=value+"%"
+    for (let i=1;i<=table_Count;i++) {
+        let obj = document.querySelector(".flex_table div:nth-child("+i+")")
+        console.log(value)
+        obj.style.width = (value*5) + "px"
+    }
+}
+function resetMainTitle(){
+    document.querySelector(".mainTitle").innerHTML=title;
+    for (let i=1;i<=table_Count;i++) {
+        document.querySelector(".flex_table div:nth-child("+i+")").textContent=i.toString()
+    }
+}
+function oninput_Count() {
+
+    let value = document.querySelector("#range_Count").value
+
+    let element = document.createElement("div")
+    for (let i = 1; i <= value; i++) {
+        document.querySelector(".flex_table").appendChild(element)
+    }
+}
+
+function onmousedown_RemoveAll() {
+    let content = document.querySelector(".flex_table")
+    content.parentNode.removeChild(content)
+}
+
 function test_Debug() {
 }
